74c6cc834f22fe2d9f72bdd68baf1a986521cb98
Ca-tt <damir96.lukyanenko@gmail.com>
2023-01-14 12:54:50 +0200

    rewrite code to promises API

--- node-app/backend/app.js
+++ node-app/backend/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import indexRoute from './route/route.js';
 import livereload from 'livereload';
 import connectLiveReload from 'connect-livereload';
+import indexRoute from './route/route.js';
 
-let app = express();
-let port = 5000;
-
+const app = express();
+const port = 5000;
 const liveReloadServer = livereload.createServer();
+
 
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
@@ -15,12 +15,12 @@ liveReloadServer.server.once("connection", () => {
 });
 
 app.use(connectLiveReload());
-app.use(express.urlencoded({ extended: true }))
-
-app.use(indexRoute);
+app.use(express.urlencoded({ extended: true })); // for req.body parsing
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+app.use(indexRoute);
 
 app.listen(port, () => {
   console.log(`the app is running on port:${port} NodeJS server
--- node-app/backend/route/route.js
+++ node-app/backend/route/route.js
@@ -1,73 +1,27 @@
 import express from 'express';
-import fs from 'node:fs';
+import { existsSync } from 'node:fs';
+import { readFile, appendFile } from 'node:fs/promises';
 
-let router = express.Router();
-
-let usersFile = './data/users.txt';
+const router = express.Router();
+const usersFile = './data/users.txt';
 
 // index route
-router.get('/', (req, res) => {
-  
-  fs.readFile(usersFile, 'utf-8', (err, data) => {
-    let users = [];
-    users = data.split('\r\n');
-    res.render('index', { content: `page res is ${res.statusCode}`, users: users });
-  })
+router.get('/', async (req, res) => {
+  const data = await readFile(usersFile, 'utf-8');
+  let users = data.split('\r\n');
+
+  res.render('index', { users });
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   let newUser = req.body.text;
-  // console.log('req: ', newUser);
-  
-  fs.readFile(usersFile, 'utf-8', (err, data) => {
-    fs.appendFile(usersFile, `\r\n${newUser}`, (err) => {
-
-    })
-  })
+  if (existsSync(usersFile)) {
+    await appendFile(usersFile, `\r\n${newUser}`);
+  }
   res.redirect('/')
-})
-
-
-// get file
-router.get('/file', (req, res) => {
-
-  fs.readFile('./data/users.txt', 'utf-8', (err, data) => {
-
-    fs.mkdir('./data/extra', () => {
-
-      fs.writeFile('./data/extra/users-copy.txt', data, (error) => {
-        error ? console.log(error) : null;
-        console.log('writing a new file...');
-      })
-
-    })
-
-    setTimeout(() => {
-      if(fs.existsSync('./data/extra/users-copy.txt')) {
-        fs.unlink('./data/extra/users-copy.txt', (err) => {
-          err ? console.log(err) : null;
-          console.log('delete the file....');
-        })
-      }
-    }, 5000)
-
-    setTimeout(() => {
-      fs.rmdir('./data/extra/', (err) => {
-        err ? console.log(err) : null;
-        console.log('remove the directory....');
-      })
-    }, 8000)
-
-
-  })
-
-  res.send('');
 })
 
 export default router;
 
-// 1. Create file
-// 2. Read the file and display it's data on page
-// 3. Write to the file's end, don't erase existing data
 
-// rewrite this app to the fetch (rest) api standard
+// 1. rewrite this app to the fetch (rest) api standard
